b388b3b64457317283ba784863773522a15a28a2
HonoredCoder <josephadjegan9@gmail.com>
2024-07-07 00:48:38 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+//pour afficher du texte
+//console.log("ceci est notre premier texte");
+//console.log("j'aime manger des nouilles");
+
+//pour afficher un message d'alert sur la fenêtre
+
+//window.alert("ceci est un message d'alert");
+//window.alert("soit flex");
+
+//document.getElementById("myH1").textContent = 'hello';
+//document.getElementById("myP").textContent = 'je debute en java Script';
+
+//declaration de variable
+
+/*let age = 23;
+let taille = 1.5;
+let leprix = 12.333;
+
+console.log(`ton age est de ${age}`);
+console.log(`ta taille est de ${taille}`);
+console.log(`le prix du produit est de ${leprix}`);
+*/
+
+// let fullName = "Honored Coder";
+// let age = 19;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = (`your full name is ${fullName}`);
+// document.getElementById("p2").textContent = `your age is ${age}`;
+// document.getElementById("p3").textContent = (`you are a student : ${isStudent}`);
+
+
+//comment demander à un utilisateur d'entrer
+
+// let username;
+
+// username = window.prompt("what's your username");
+// console.log(username);
+
+// let Username;
+
+// document.getElementById("mySubmit").onclick = function () {
+//     Username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `Hello ${Username}`;
+// }
+
+// conversion des types des variables
+
+
+// let age = window.prompt("how old are you");
+// age = Number(age);
+
+// age += 1;
+
+// console.log(age);19
+
+
+//comment déclarer les constantes
+
+// const pi = 3.14159;
+
+// const PI = 3.14159;
+// let radius;
+// let cir;
+
+
+// document.getElementById("mySubmit").onclick = function () {
+//     radius = document.getElementById("myText").value;
+//     radius = Number(radius);
+//     cir = 2 * PI * radius;
+//     document.getElementById("myResult").textContent = cir;
+// };
+
+
+//programme de joseph du compteur
+
+// let nombre;
+// document.getElementById("increase").onclick = function () {
+//     nombre = document.getElementById("myNumber").textContent = 1;
+//     nombre = Number(nombre);
+    
+//     document.getElementById("increase").onclick = function () {
+//         nombre++;
+//         document.getElementById("myNumber").textContent = nombre;
+
+//     }
+// }
+
+// document.getElementById("decrease").onclick = function () {
+//     nombre = document.getElementById("myNumber").textContent = 1;
+//     nombre = Number(nombre);
+
+//     document.getElementById("decrease").onclick = function () {
+//         nombre--;
+//         document.getElementById("myNumber").textContent = nombre;
+
+//     }
+// }
+// document.getElementById("reset").onclick = function () {
+//     nombre = document.getElementById("myNumber").textContent =0 ;
+// }
+
+
+//la mathématique
+
+/*Cette fonction renvoie l'entier le plus proche
+ d'un nombre donné. Si la partie décimale est de 
+ 0.5 ou plus, le nombre est arrondi vers le haut. 
+ Sinon, il est arrondi vers le bas.*/
+
+//x = Math.round(3.3);
+
+/*Math.floor()
+Cette fonction renvoie le plus grand entier
+ inférieur ou égal à un nombre donné. Elle 
+ arrondit toujours vers le bas.*/
+ 
+//x = Math.floor();
+
+//La fonction Math.ceil() en JavaScript renvoie
+//le plus petit entier supérieur ou égal à un nombre
+//donné.Elle permet d'arrondir un nombre à l'entier
+//le plus proche supérieur ou égal à ce nombre.
+//let x;
+//x = Math.ceil(3.21);
+
+//x = Math.pow();
+
+
+//console.log(x);
+
+
+
+
